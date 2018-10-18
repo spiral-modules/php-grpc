0954db6be9f7102967169a05144dbd9d6add730c
Wolfy-J <wolfy.jd@gmail.com>
2018-10-18 19:22:19 +0300

    fix namespace

--- example/src/Service/EchoService.php
+++ example/src/Service/EchoService.php
@@ -15,8 +15,6 @@ class EchoService implements EchoInterface
 {
     public function Ping(ContextInterface $ctx, Message $in): Message
     {
-        error_log(print_r($ctx, 1));
-
         return $in;
     }
 }
