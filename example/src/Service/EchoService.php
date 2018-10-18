f32ebdd2d1703ff196db44fa64656ea0616776a5
Wolfy-J <wolfy.jd@gmail.com>
2018-10-18 19:10:59 +0300

    example

---
+++ example/src/Service/EchoService.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Service;
+
+
+use Spiral\GRPC\ContextInterface;
+
+class EchoService implements EchoInterface
+{
+    public function Ping(ContextInterface $ctx, Message $in): Message
+    {
+        error_log(print_r($ctx, 1));
+
+        return $in;
+    }
+}
